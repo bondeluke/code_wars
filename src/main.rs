6c47e88e1ee440f2e1d416dc4a5d48369b3b295e
Luke Bonde <bondeluke@gmail.com>
2024-02-14 13:37:58 -0600

    alphabet_position

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod to_camel_case;
 mod find_short;
 mod is_pangram;
 mod multiples_of_3_or_5;
+mod alphabet_position;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", "Hello, world!");
 }
