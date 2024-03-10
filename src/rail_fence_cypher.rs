5351b7665acdc09e83d2d5f79cf8909c0b41b447
Luke Bonde <bondeluke@gmail.com>
2024-03-09 23:21:01 -0600

    rail cypher encode working and much cleaner

--- src/rail_fence_cypher.rs
+++ src/rail_fence_cypher.rs
@@ -1,48 +1,28 @@
 // https://www.codewars.com/kata/58c5577d61aefcf3ff000081/train/rust
 
 fn encode_rail_fence_cipher(text: &str, num_rails: usize) -> String {
-    if text == "" { return text.to_string(); }
+    let length = text.len();
+    let cycle_length = (num_rails - 1) * 2;
+    let mut encoded_text = String::new();
 
-    let chars = text.chars();
-    let length = chars.count();
-    let cycle = (num_rails - 1) * 2;
-    let mut answer = "".to_string();
-    let mut index = 0;
-
-    // Handle the 0-th rail
-    while index < length {
-        answer.push_str(&text[index..index + 1]);
-        index += cycle;
-    }
-
-    // Handle the middle rails
-    for rail in 1..num_rails - 1 {
-        let (mut first_index, mut second_index) = (rail, cycle - rail);
-        loop {
-            if first_index < length {
-                answer.push_str(&text[first_index..first_index + 1]);
-                first_index += cycle;
-            } else { break; }
-            if second_index < length {
-                answer.push_str(&text[second_index..second_index + 1]);
-                second_index += cycle;
-            } else { break; }
+    for rail in 0..num_rails {
+        for index in (rail..length).step_by(cycle_length) {
+            encoded_text.push_str(&text[index..=index]);
+            if rail != 0 && rail != num_rails - 1 {
+                let index2 = index + (cycle_length - 2 * rail);
+                if index2 < length {
+                    encoded_text.push_str(&text[index2..=index2]);
+                }
+            }
         }
     }
 
-    // Handle the last rail
-    index = cycle / 2;
-    while index < length {
-        answer.push_str(&text[index..index + 1]);
-        index += cycle;
-    }
-
-    return answer;
+    return encoded_text;
 }
 
-fn decode_rail_fence_cipher(text: &str, num_rails: usize) -> String {
-    todo!()
-}
+// fn decode_rail_fence_cipher(text: &str, num_rails: usize) -> String {
+//     todo!()
+// }
 
 #[cfg(test)]
 mod tests {
