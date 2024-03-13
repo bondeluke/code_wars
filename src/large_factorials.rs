5dc39f03610e80f69f444ab8dcd73eefd26a5a78
Luke Bonde <bondeluke@gmail.com>
2024-03-13 12:22:48 -0500

    cleaner, more concise logic for factorial(x: u32)

--- src/large_factorials.rs
+++ src/large_factorials.rs
@@ -1,44 +1,35 @@
 //https://www.codewars.com/kata/557f6437bf8dcdd135000010/train/rust
 
-fn get_base(x: u64) -> (u64, usize) {
-    let limit: u64 = 18446744073709551615 / (x + 1);
-
-    let mut base = 10_u64;
-    let mut exp = 1;
-
-    while base * 10 < limit {
-        base *= 10;
-        exp += 1
-    }
-
-    (base, exp)
-}
-
 #[allow(dead_code)]
-fn factorial(x: u32) -> String {
-    let x = x as u64;
-    let (base, exp) = get_base(x);
-    let mut result = vec![1u64];
+pub fn factorial(x: u32) -> String {
+    if x == 0 { return String::from("1"); }
 
+    // Pick the largest base such that x * base < u64::MAX
+    // to ensure that no products overflow and carry < base
+    let x = x as u64;
+    let exp = ((u64::MAX / x) as f64).log10() as usize;
+    let base = 10_u64.pow(exp as u32);
+
+    let mut result = vec![1];
     for factor in 2..=x {
+        let mut carry = 0;
+
         for part in result.iter_mut() {
-            *part *= factor;
+            let product = *part * factor + carry;
+            *part = product % base;
+            carry = product / base;
         }
-        for i in 0..result.len() {
-            while result[i] >= base {
-                result[i] -= base;
-                if i == result.len() - 1 {
-                    result.push(0)
-                }
-                result[i + 1] += 1;
-            }
+
+        if carry > 0 {
+            result.push(carry);
         }
     }
 
-    result.iter().rev().enumerate().map(|(i, &p)| match p {
-        0 => "0".repeat(exp),
-        _ => if i == 0 { p.to_string() } else { format!("{p:0exp$}") }
-    }).collect::<Vec<String>>().join("")
+    result.iter().rev().enumerate().map(|x| match x {
+        (_, 0) => "0".repeat(exp),
+        (0, p) => p.to_string(),
+        (_, p) => format!("{p:0exp$}")
+    }).collect::<String>()
 }
 
 #[cfg(test)]
@@ -47,7 +38,11 @@ mod tests {
 
     #[test]
     fn sample_tests() {
+        assert_eq!(factorial(0), String::from("1"));
         assert_eq!(factorial(1), String::from("1"));
+        assert_eq!(factorial(2), String::from("2"));
+        assert_eq!(factorial(3), String::from("6"));
+        assert_eq!(factorial(4), String::from("24"));
         assert_eq!(factorial(5), String::from("120"));
         assert_eq!(factorial(9), String::from("362880"));
         assert_eq!(factorial(15), String::from("1307674368000"));
