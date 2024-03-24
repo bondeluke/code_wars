89e3e46a89c903cf67fd49ff57eda1afff023b66
Luke Bonde <bondeluke@gmail.com>
2024-03-24 15:01:06 -0500

    The wheel is turning!

--- src/prime_streaming_nc17.rs
+++ src/prime_streaming_nc17.rs
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/59122604e5bc240817000016
 
-use std::cmp::max;
+use std::cmp::{max};
 use std::iter::once;
 
 pub fn stream17() -> impl Iterator<Item=u32> {
@@ -8,22 +8,22 @@ pub fn stream17() -> impl Iterator<Item=u32> {
 }
 
 #[derive(Clone)]
-pub struct Wheel {
-    pub basis: Vec<u32>,
-    pub spokes: Vec<u32>,
+struct Wheel {
+    pub basis: Vec<usize>,
+    pub spokes: Vec<usize>,
 }
 
 impl Wheel {
-    pub fn circumference(&self) -> u32 {
+    pub fn circumference(&self) -> usize {
         self.basis.iter().product()
     }
 }
 
-pub fn next_wheel(wheel: Wheel) -> Wheel {
+fn next_wheel(wheel: Wheel) -> Wheel {
     let p = if wheel.spokes.len() > 1 { wheel.spokes[1] } else { 3 };
     let circ = wheel.circumference();
-    let basis = wheel.basis.iter().copied().chain(once(p)).collect::<Vec<u32>>();
-    let mut spokes: Vec<u32> = vec![];
+    let basis = wheel.basis.iter().copied().chain(once(p)).collect::<Vec<usize>>();
+    let mut spokes: Vec<usize> = vec![];
     for k in 0..p {
         for s in &wheel.spokes {
             let spoke = k * circ + s;
@@ -35,7 +35,7 @@ pub fn next_wheel(wheel: Wheel) -> Wheel {
     Wheel { basis, spokes }
 }
 
-pub fn get_wheel(basis_size: u32) -> Wheel {
+fn get_wheel(basis_size: usize) -> Wheel {
     let mut wheel = Wheel { basis: vec![2], spokes: vec![1] };
     for _ in 1..basis_size {
         wheel = next_wheel(wheel);
@@ -45,38 +45,61 @@ pub fn get_wheel(basis_size: u32) -> Wheel {
 
 struct PrimeIterator {
     cursor: i32,
-    primes: Vec<u32>,
+    primes: Vec<usize>,
     wheel: Wheel,
     segment: usize,
+    next_spoke: Vec<usize>,
+    sieve: Vec<bool>,
 }
 
 impl PrimeIterator {
     fn new() -> Self {
         Self {
-            cursor: -1,
+            wheel: get_wheel(4),
+            sieve: vec![],
+            next_spoke: vec![],
             primes: vec![],
-            wheel: get_wheel(7),
             segment: 1,
+            cursor: -1,
         }
     }
 
-    fn initialize(&mut self) {
+    fn initialize_caches(&mut self) {
+        let circ = self.wheel.circumference();
+
+        // Next spoke lookup
+        self.next_spoke = vec![0; circ];
+        let mut spoke_index = 0;
+        for i in 0..circ {
+            let spoke = self.wheel.spokes[spoke_index];
+            self.next_spoke[i] = spoke_index;
+            if i == spoke {
+                spoke_index += 1;
+            }
+        }
+
+        // Copyable sieve
+        self.sieve = vec![false; circ / 3];
+        for &spoke in &self.wheel.spokes {
+            self.sieve[spoke / 3] = true;
+        }
+    }
+
+    fn initialize_primes(&mut self) {
         let spokes = &self.wheel.spokes;
+        let circ = self.wheel.circumference();
         let limit = self.wheel.circumference();
         let basis_size = self.wheel.basis.len();
         let spokes_len = spokes.len();
 
-        println!("Initializing primes up to {}...", self.wheel.circumference());
+        println!("Initializing primes up to {}...", circ);
 
         // (1) Start with the basis and the first non-1 spoke
         self.primes.extend(&self.wheel.basis);
         self.primes.push(spokes[1]);
 
         // (2) Create a sieve to track spoke primality
-        let mut sieve: Vec<bool> = vec![false; (self.wheel.circumference() / 3) as usize];
-        for &spoke in &self.wheel.spokes {
-            sieve[(spoke / 3) as usize] = true;
-        }
+        let mut sieve = self.sieve.clone();
 
         // (3) Cross out composites using spoke multiples
         let mut j_lb = 1;
@@ -96,12 +119,12 @@ impl PrimeIterator {
                 let product = prime * spoke;
                 if product > limit { break; }
 
-                sieve[(product / 3) as usize] = false;
+                sieve[product / 3] = false;
                 //println!("Crossing out {} x {} = {} at index {}", prime, spoke, product, product / 3);
             }
 
             while spokes[spoke_index] < p_squared {
-                if sieve[(spokes[spoke_index] / 3) as usize] {
+                if sieve[spokes[spoke_index] / 3] {
                     //println!("Adding {spoke} as a prime");
                     self.primes.push(spokes[spoke_index]);
                 }
@@ -113,50 +136,59 @@ impl PrimeIterator {
 
         // (4) Add remaining spokes that have not been crossed out
         for &spoke in &spokes[spoke_index..spokes_len] {
-            if sieve[(spoke / 3) as usize] {
+            if sieve[spoke / 3] {
                 //println!("Adding {spoke} as a prime (leftover)");
                 self.primes.push(spoke);
             }
         }
 
-        //println!("Initialized {} primes. Last prime added was {}", self.primes.len(), self.primes[self.primes.len() - 1])
+        println!("Initialized {} primes. Last prime added was {}", self.primes.len(), self.primes[self.primes.len() - 1])
     }
 
     fn extend(&mut self) {
         let spokes = &self.wheel.spokes;
-        let circ = self.wheel.circumference() as usize;
+        let circ = self.wheel.circumference();
         let lower_limit = self.segment * circ;
         let upper_limit = (self.segment + 1) * circ;
         let basis_size = self.wheel.basis.len();
 
-        //println!("Expanding primes in range {} - {}...", lower_limit, upper_limit);
+        println!("Expanding primes in range {} - {}...", lower_limit, upper_limit);
 
-        // (2) Create a sieve to track spoke primality
-        let mut sieve: Vec<bool> = vec![false; circ / 2];
-        for &spoke in &self.wheel.spokes {
-            sieve[(spoke / 2) as usize] = true;
-        }
+        // (1) Create a sieve to track spoke primality
+        let mut sieve = self.sieve.clone();
 
-        // (3) Cross out composites using spoke multiples
-        for i in basis_size.. {
-            let prime = self.primes[i] as usize;
+        // (2) Cross out composites using spoke multiples
+        for &prime in &self.primes[basis_size..] {
             let p_squared = prime * prime;
             if p_squared > upper_limit { break; }
 
-            let low = ((lower_limit - 1) / prime + 1) * prime;
-            let odd_low = if low % 2 == 0 { low + prime } else { low };
-            let start = max(odd_low, p_squared);
-            for n in (start..upper_limit).step_by(prime * 2) {
-                // println!("Crossing out {} x {} = {} at index {}", prime, n / prime, n, n - lower_limit);
-                sieve[(n - lower_limit) / 2] = false;
+            // prime * (k * circ + s_prime) > lower_limit
+            let lowest_factor = max((lower_limit - 1) / prime + 1, prime);
+            let s_prime = lowest_factor % circ;
+            let mut k = lowest_factor / circ;
+            let mut spoke_index = self.next_spoke[s_prime];
+            loop {
+                let spoke = spokes[spoke_index];
+                let n = prime * (k * circ + spoke);
+                if n > upper_limit { break; }
+
+                println!("Crossing out {prime} x ({k} * {circ} + {spoke}) = {n} at index {}", n - lower_limit);
+                sieve[(n - lower_limit) / 3] = false;
+                if spoke_index < spokes.len() - 1 {
+                    spoke_index += 1
+                } else {
+                    println!("This actually happened!");
+                    k += 1;
+                    spoke_index = 0;
+                }
             }
         }
 
-        // (4) Add remaining spokes that have not been crossed out
+        // (3) Add primes for the spokes which have not been crossed out
         for &spoke in spokes {
-            if sieve[(spoke / 2) as usize] {
-                //println!("Adding {spoke} as a prime (leftover)");
-                self.primes.push((lower_limit as u32) + spoke);
+            if sieve[spoke / 3] {
+                //println!("Adding {spoke} as a prime");
+                self.primes.push(lower_limit + spoke);
             }
         }
         self.segment += 1;
@@ -171,13 +203,14 @@ impl Iterator for PrimeIterator {
 
         if self.cursor as usize == self.primes.len() {
             if self.primes.is_empty() {
-                self.initialize()
+                self.initialize_caches();
+                self.initialize_primes();
             } else {
                 self.extend();
             }
         }
 
-        Some(self.primes[self.cursor as usize])
+        Some(self.primes[self.cursor as usize] as u32)
     }
 }
 
